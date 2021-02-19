d42a39b4282b81d0fd7378487d2f700368770b08
amarg6413 <47099838+amarg6413@users.noreply.github.com>
2021-02-19 19:32:39 +0530

    Add files via upload

---
+++ maxpath.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream> 
+using namespace std; 
+
+int numberOfPaths(int m, int n) 
+{ 
+	if (m == 1 || n == 1) 
+		return 1; 
+	return numberOfPaths(m - 1, n) + numberOfPaths(m, n - 1); 
+} 
+
+int main() 
+{ 
+    int m,n;
+    cinn>>m>>n;
+	cout << numberOfPaths(3, 3); 
+	return 0; 
+} 
